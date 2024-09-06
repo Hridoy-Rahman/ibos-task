6ab93e2120f2d4d58ea3defeded3fb757fecaa2b
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-09-06 10:55:32 +0600

    navbar done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar/>
             <Footer/>
         </div>
     );
